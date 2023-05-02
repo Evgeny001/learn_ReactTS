f4c8f68e022aec05e0b2edc9a3a67f1df0a1b876
EvgenyLukashevich <info.elukas@gmail.com>
2023-05-03 00:11:44 +0300

    Lesson_03

---
+++ src/Components /Rating/Rating.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+
+export const Rating = (props: any) => {
+    console.log()
+    if (props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>)
+}
+
+export const Star = (props: any) => {
+    debugger
+    console.log("star rendering") //добавляя console.log в console браузера можно увидеть что программа отработала
+    if (props.selected === true)
+        return (
+            <span><b>Star </b></span>
+        )
+    else
+        return <span>Star</span>
+}
